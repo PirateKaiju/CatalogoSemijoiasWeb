53453bbdd0696d9ff227b4d29389b8bc05f2024d
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-04-11 20:24:43 -0300

    Product templates and navbar

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class HomeController extends AbstractController{
+
+        /**
+         * @Route("/", methods={"GET"})
+         */
+
+        public function index(){
+
+            return $this->redirectToRoute("product_index");
+
+        }
+
+    }
+
+?>
